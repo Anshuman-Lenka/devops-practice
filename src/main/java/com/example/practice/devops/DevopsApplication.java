f639c0b63f31d93e8918b98125a3ccddf3899265
Anshuman Lenka <anshumanlenka7@gmail.com>
2024-05-05 14:59:51 +0530

    Initial commit

---
+++ src/main/java/com/example/practice/devops/DevopsApplication.java
@@ -0,0 +1,18 @@
+package com.example.practice.devops;
+
+import java.util.Date;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hi, How are you.....");
+		System.out.println("Sostware Engineer.....");
+		System.out.println(new Date());
+
+	}
+
+}
